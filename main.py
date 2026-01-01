a9504f8d8fb5dbfde41726260d5b8b81eb29c92d
MUHAMMAD MAHTAB FAISAL <cursedloop325@gmail.com>
2026-01-01 21:40:33 +0500

    Created Main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+# main.py
 
+from data import MovieData
+from admin import Admin
+from user import User
+
+data = MovieData()
+admin = Admin(data)
+user = User(data)
+
+while True:
+    print("\n1. Admin Mode\n2. User Mode\n3. Exit")
+    ch = input("Choose: ")
+
+    if ch == "1":
+        print("1.View 2.Add 3.Remove")
+        a = input("Choice: ")
+        if a == "1": admin.view_movies()
+        elif a == "2": admin.add_movie()
+        elif a == "3": admin.remove_movie()
+
+    elif ch == "2":
+        user.book_ticket()
+
+    elif ch == "3":
+        break
